9fa1d4ce63b00e7754b2928f8d188dc25f526de4
Moaaz AbdEl-Salam <moaazabdelsalam99@gmail.com>
2023-09-05 16:10:21 +0300

    implement button actions for screen transition

--- Client/src/screens/LocalPlayersNamesUI.java
+++ Client/src/screens/LocalPlayersNamesUI.java
@@ -1,6 +1,6 @@
 package screens;
 
-
+import client.Constants;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -10,72 +10,91 @@ public class LocalPlayersNamesUI extends AnchorPane {
 
     protected final Label label;
     protected final Label label0;
-    protected final TextField tfPlayerOneName;
-    protected final TextField tfPlayerTwoName;
     protected final Button btnStart;
     protected final Button btnCancel;
+    protected final TextField player1TextField;
+    protected final TextField player2TextField;
 
     public LocalPlayersNamesUI() {
 
         label = new Label();
         label0 = new Label();
-        tfPlayerOneName = new TextField();
-        tfPlayerTwoName = new TextField();
         btnStart = new Button();
         btnCancel = new Button();
+        player1TextField = new TextField();
+        player2TextField = new TextField();
 
         setId("AnchorPane");
-        setPrefHeight(263.0);
-        setPrefWidth(401.0);
+        setPrefHeight(400.0);
+        setPrefWidth(600.0);
         getStyleClass().add("regbg-pane");
-        getStylesheets().add("/screens/../resources/regbg.css");
+        getStylesheets().add(Constants.regbgCSSPath.toUri().toString());
 
-        label.setLayoutX(55.0);
-        label.setLayoutY(53.0);
+        label.setLayoutX(118.0);
+        label.setLayoutY(118.0);
         label.setPrefHeight(34.0);
         label.setPrefWidth(110.0);
         label.getStyleClass().add("custom-label");
-        label.getStylesheets().add("/screens/../resources/labels.css");
+        label.getStylesheets().add(Constants.labelsCSSPath.toUri().toString());
         label.setText("Player #1 Name");
 
-        label0.setLayoutX(55.0);
-        label0.setLayoutY(90.0);
+        label0.setLayoutX(118.0);
+        label0.setLayoutY(167.0);
         label0.setPrefHeight(34.0);
         label0.setPrefWidth(110.0);
         label0.getStyleClass().add("custom-label");
-        label0.getStylesheets().add("/screens/../resources/labels.css");
+        label0.getStylesheets().add(Constants.labelsCSSPath.toUri().toString());
         label0.setText("Player #2 Name");
 
-        tfPlayerOneName.setLayoutX(173.0);
-        tfPlayerOneName.setLayoutY(57.0);
-
-        tfPlayerTwoName.setLayoutX(173.0);
-        tfPlayerTwoName.setLayoutY(95.0);
-
-        btnStart.setLayoutX(55.0);
-        btnStart.setLayoutY(169.0);
+        btnStart.setLayoutX(118.0);
+        btnStart.setLayoutY(298.0);
         btnStart.setMnemonicParsing(false);
         btnStart.setPrefHeight(25.0);
         btnStart.setPrefWidth(110.0);
         btnStart.getStyleClass().add("custom-button-large");
-        btnStart.getStylesheets().add("/screens/../resources/buttons.css");
+        btnStart.getStylesheets().add(Constants.buttonsCSSPath.toUri().toString());
         btnStart.setText("START");
 
-        btnCancel.setLayoutX(242.0);
-        btnCancel.setLayoutY(169.0);
+        btnCancel.setLayoutX(393.0);
+        btnCancel.setLayoutY(298.0);
         btnCancel.setMnemonicParsing(false);
         btnCancel.setPrefHeight(53.0);
         btnCancel.setPrefWidth(116.0);
         btnCancel.getStyleClass().add("custom-button-large");
-        btnCancel.getStylesheets().add("/screens/../resources/buttons.css");
+        btnCancel.getStylesheets().add(Constants.buttonsCSSPath.toUri().toString());
         btnCancel.setText("Cancel");
+
+        player1TextField.setLayoutX(300.0);
+        player1TextField.setLayoutY(120.0);
+        player1TextField.setPromptText("Enter First Player Name");
+
+        player2TextField.setLayoutX(300.0);
+        player2TextField.setLayoutY(168.0);
+        player2TextField.setPromptText("Enter Second Player Name");
 
         getChildren().add(label);
         getChildren().add(label0);
-        getChildren().add(tfPlayerOneName);
-        getChildren().add(tfPlayerTwoName);
         getChildren().add(btnStart);
         getChildren().add(btnCancel);
+        getChildren().add(player1TextField);
+        getChildren().add(player2TextField);
 
     }
+
+    public Button getBtnStart() {
+        return btnStart;
+    }
+
+    public Button getBtnCancel() {
+        return btnCancel;
+    }
+
+    public TextField getPlayer1TextField() {
+        return player1TextField;
+    }
+
+    public TextField getPlayer2TextField() {
+        return player2TextField;
+    }
+    
 }
