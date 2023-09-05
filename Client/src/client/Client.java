2090b7b4d1ccc5bf37c00850286d5aeb9bbcbb69
Abdullah Ali Mohamed <Abdallah2ali2020@gmail.com>
2023-09-05 06:35:02 +0300

    Add Profile-Login/Register-LocalPlayersNames screens to client app

--- Client/src/client/Client.java
+++ Client/src/client/Client.java
@@ -1,4 +1,3 @@
-
 package client;
 
 import javafx.application.Application;
@@ -6,27 +5,35 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import screens.AfterGameScreenUI;
 import screens.GameScreenUI;
-
+import screens.LocalPlayersNamesUI;
+import screens.LoginRegisterUI;
+import screens.PlayerProfileUI;
 
 public class Client extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
-        GameScreenUI root = new GameScreenUI();
+        //LocalPlayersNamesUI root = new LocalPlayersNamesUI();
+        PlayerProfileUI root = new PlayerProfileUI();
         //AfterGameScreenUI root = new AfterGameScreenUI();
-        
-                
+
         Scene scene = new Scene(root);
-        
+
         scene.getStylesheets().add(getClass().getResource("style.css").toString());
+        scene.getStylesheets().add(getClass().getResource("/resources/buttons.css").toString());
+        scene.getStylesheets().add(getClass().getResource("/resources/background.css").toString());
+        scene.getStylesheets().add(getClass().getResource("/resources/labels.css").toString());
+        scene.getStylesheets().add(getClass().getResource("/resources/regbg.css").toString());
+        scene.getStylesheets().add(getClass().getResource("/resources/transparentButton.css").toString());
+
         stage.setScene(scene);
         stage.show();
-        
+
         //  root.mediaViewer.setMediaPlayer(mediaPlayer);
     }
 
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
