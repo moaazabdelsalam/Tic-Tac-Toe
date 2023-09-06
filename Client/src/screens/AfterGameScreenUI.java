fbbc7be8a509168a9ed0bc9a99ae903b0bb9e650
Omar El Samahy <eng.omaryasser@gmail.com>
2023-09-06 18:20:43 +0300

    Unified Layouts & Added Back Button

--- Client/src/screens/AfterGameScreenUI.java
+++ Client/src/screens/AfterGameScreenUI.java
@@ -1,42 +1,24 @@
 package screens;
 
-import java.io.File;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 
 public class AfterGameScreenUI extends BorderPane {
 
     protected final MediaView mediaViewer;
-    protected final HBox hBox;
-    protected final Button playAgainBtn;
+    protected final AnchorPane anchorPane;
     protected final Button ExitBtn;
+    protected final Button playAgainBtn;
 
     public AfterGameScreenUI() {
-        
-        MediaPlayer mediaPlayer;
-        /*//File file = new File("F:\\iti\\Developing Applications using Java SE\\Project\\Tic Tac Toe\\Client\\src\\resources\\success1.mp4");
-        Path absolutePath1 = Paths.get("F:\\iti\\Developing Applications using Java SE\\Project\\Tic Tac Toe\\Client\\src\\resources\\success1.mp4");
-        Path absolutePath2 = Paths.get("F:\\iti\\Developing Applications using Java SE\\Project\\Tic Tac Toe\\Client");
 
-        // convert the absolute path to relative path
-        Path relativePath = absolutePath2.relativize(absolutePath1);
-        System.out.println(relativePath);*/
-        File file = new File("src\\resources\\success1.mp4");
-        Media media = new Media(file.toURI().toString());
-        mediaPlayer = new MediaPlayer(media);
-        mediaViewer = new MediaView(mediaPlayer);
-        mediaViewer.setPreserveRatio(true);
-        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
-        mediaPlayer.play();
-        
-        hBox = new HBox();
-        playAgainBtn = new Button();
+        mediaViewer = new MediaView();
+        anchorPane = new AnchorPane();
         ExitBtn = new Button();
+        playAgainBtn = new Button();
 
         setMaxHeight(USE_PREF_SIZE);
         setMaxWidth(USE_PREF_SIZE);
@@ -44,34 +26,37 @@ public class AfterGameScreenUI extends BorderPane {
         setMinWidth(USE_PREF_SIZE);
         setPrefHeight(400.0);
         setPrefWidth(600.0);
+        getStyleClass().add("regbg-pane");
+        getStylesheets().add("/screens/../resources/regbg.css");
 
         BorderPane.setAlignment(mediaViewer, javafx.geometry.Pos.CENTER);
-        
+        mediaViewer.setFitHeight(200.0);
+        mediaViewer.setFitWidth(200.0);
         BorderPane.setMargin(mediaViewer, new Insets(0.0));
-        mediaViewer.setFitWidth(500);
-        mediaViewer.setFitHeight(300);
         setCenter(mediaViewer);
 
-        BorderPane.setAlignment(hBox, javafx.geometry.Pos.CENTER);
-        hBox.setAlignment(javafx.geometry.Pos.CENTER);
-        hBox.setPrefHeight(0.0);
-        hBox.setPrefWidth(0.0);
-
-        playAgainBtn.setMnemonicParsing(false);
-        playAgainBtn.setText("Play Again");
+        BorderPane.setAlignment(anchorPane, javafx.geometry.Pos.CENTER);
+        BorderPane.setMargin(anchorPane, new Insets(0.0, 0.0, 16.0, 0.0));
 
         ExitBtn.setAlignment(javafx.geometry.Pos.CENTER);
         ExitBtn.setContentDisplay(javafx.scene.control.ContentDisplay.CENTER);
+        ExitBtn.setLayoutX(356.0);
         ExitBtn.setMnemonicParsing(false);
         ExitBtn.setPrefHeight(25.0);
         ExitBtn.setPrefWidth(66.0);
+        ExitBtn.getStyleClass().add("custom-button");
+        ExitBtn.getStylesheets().add("/screens/../resources/buttons.css");
         ExitBtn.setText("Exit");
-        BorderPane.setMargin(hBox, new Insets(0.0, 0.0, 16.0, 0.0));
-        //BorderPane.setMargin(mediaViewer, new Insets(16.0, 16.0, 16.0, 16.0));
-        setBottom(hBox);
 
-        hBox.getChildren().add(playAgainBtn);
-        hBox.getChildren().add(ExitBtn);
+        playAgainBtn.setLayoutX(159.0);
+        playAgainBtn.setMnemonicParsing(false);
+        playAgainBtn.getStyleClass().add("custom-button");
+        playAgainBtn.getStylesheets().add("/screens/../resources/buttons.css");
+        playAgainBtn.setText("Play Again");
+        setBottom(anchorPane);
+
+        anchorPane.getChildren().add(ExitBtn);
+        anchorPane.getChildren().add(playAgainBtn);
 
     }
 }
---
+++ Client/src/screens/LocalPlayersNamesUI.java
@@ -0,0 +1,133 @@
+package screens;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+public class LocalPlayersNamesUI extends BorderPane {
+
+    protected final AnchorPane anchorPane;
+    protected final Label label;
+    protected final Label label0;
+    protected final TextField player1TextField;
+    protected final TextField player2TextField;
+    protected final AnchorPane anchorPane0;
+    protected final Button btnStart;
+    protected final Button btnCancel;
+    protected final Button backBtn;
+    protected final ImageView imageView;
+
+    public LocalPlayersNamesUI() {
+
+        anchorPane = new AnchorPane();
+        label = new Label();
+        label0 = new Label();
+        player1TextField = new TextField();
+        player2TextField = new TextField();
+        anchorPane0 = new AnchorPane();
+        btnStart = new Button();
+        btnCancel = new Button();
+        backBtn = new Button();
+        imageView = new ImageView();
+
+        setMaxHeight(USE_PREF_SIZE);
+        setMaxWidth(USE_PREF_SIZE);
+        setMinHeight(USE_PREF_SIZE);
+        setMinWidth(USE_PREF_SIZE);
+        setPrefHeight(400.0);
+        setPrefWidth(600.0);
+        getStyleClass().add("regbg-pane");
+        getStylesheets().add("/screens/../resources/regbg.css");
+
+        BorderPane.setAlignment(anchorPane, javafx.geometry.Pos.CENTER);
+        anchorPane.setId("AnchorPane");
+        anchorPane.setPrefHeight(118.0);
+        anchorPane.setPrefWidth(600.0);
+
+        label.setLayoutX(116.0);
+        label.setLayoutY(35.0);
+        label.setPrefHeight(34.0);
+        label.setPrefWidth(110.0);
+        label.getStyleClass().add("custom-label");
+        label.getStylesheets().add("/screens/../resources/labels.css");
+        label.setText("Player #1 Name");
+
+        label0.setLayoutX(114.0);
+        label0.setLayoutY(121.0);
+        label0.setPrefHeight(34.0);
+        label0.setPrefWidth(110.0);
+        label0.getStyleClass().add("custom-label");
+        label0.getStylesheets().add("/screens/../resources/labels.css");
+        label0.setText("Player #2 Name");
+
+        player1TextField.setLayoutX(298.0);
+        player1TextField.setLayoutY(37.0);
+        player1TextField.setPromptText("Enter First Player Name");
+
+        player2TextField.setLayoutX(296.0);
+        player2TextField.setLayoutY(122.0);
+        player2TextField.setPromptText("Enter Second Player Name");
+        setCenter(anchorPane);
+
+        BorderPane.setAlignment(anchorPane0, javafx.geometry.Pos.CENTER);
+        anchorPane0.setPrefHeight(116.0);
+        anchorPane0.setPrefWidth(600.0);
+
+        btnStart.setLayoutX(154.0);
+        btnStart.setLayoutY(44.0);
+        btnStart.setMnemonicParsing(false);
+        btnStart.setPrefHeight(25.0);
+        btnStart.setPrefWidth(110.0);
+        btnStart.getStyleClass().add("custom-button-large");
+        btnStart.getStylesheets().add("/screens/../resources/buttons.css");
+        btnStart.setText("START");
+
+        btnCancel.setLayoutX(352.0);
+        btnCancel.setLayoutY(44.0);
+        btnCancel.setMaxHeight(USE_PREF_SIZE);
+        btnCancel.setMaxWidth(USE_PREF_SIZE);
+        btnCancel.setMinHeight(USE_PREF_SIZE);
+        btnCancel.setMinWidth(USE_PREF_SIZE);
+        btnCancel.setMnemonicParsing(false);
+        btnCancel.setPrefHeight(53.0);
+        btnCancel.setPrefWidth(116.0);
+        btnCancel.getStyleClass().add("custom-button-large");
+        btnCancel.getStylesheets().add("/screens/../resources/buttons.css");
+        btnCancel.setText("Cancel");
+        setBottom(anchorPane0);
+        setPadding(new Insets(5.0));
+
+        BorderPane.setAlignment(backBtn, javafx.geometry.Pos.CENTER_LEFT);
+        backBtn.setContentDisplay(javafx.scene.control.ContentDisplay.GRAPHIC_ONLY);
+        backBtn.setMaxHeight(USE_PREF_SIZE);
+        backBtn.setMaxWidth(USE_PREF_SIZE);
+        backBtn.setMinHeight(USE_PREF_SIZE);
+        backBtn.setMinWidth(USE_PREF_SIZE);
+        backBtn.setMnemonicParsing(false);
+        backBtn.setPrefHeight(50.0);
+        backBtn.setPrefWidth(50.0);
+        backBtn.getStyleClass().add("transparent-button");
+        backBtn.getStylesheets().add("/screens/../resources/transparentButton.css");
+        backBtn.setText("Back");
+
+        imageView.setFitHeight(50.0);
+        imageView.setFitWidth(50.0);
+        imageView.setImage(new Image(getClass().getResource("../resources/backArrow.png").toExternalForm()));
+        backBtn.setGraphic(imageView);
+        BorderPane.setMargin(backBtn, new Insets(15.0));
+        setTop(backBtn);
+
+        anchorPane.getChildren().add(label);
+        anchorPane.getChildren().add(label0);
+        anchorPane.getChildren().add(player1TextField);
+        anchorPane.getChildren().add(player2TextField);
+        anchorPane0.getChildren().add(btnStart);
+        anchorPane0.getChildren().add(btnCancel);
+
+    }
+}
